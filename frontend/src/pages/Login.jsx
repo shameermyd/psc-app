802486dcd770a703ed728d2b760f61e43ab916cb
shameer <shameer.myd@gmail.com>
2026-04-02 11:22:51 +0530

    Add login state and frontend api setup

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+import { loginApi } from "../services/Api";
+
 const Login = () => {
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
+    const handleLogIn = async() =>{
+        console.log(email,password);
+        try {
+            const response = await loginApi({ email, password });
+            console.log("Response",response);
+            alert("Login Successfully")
+        } catch (error) {
+            console.error(error);
+            alert("Login Failed!!");
+        }
+    }
+
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="border p-6 rounded shadow w-80">
@@ -8,15 +26,22 @@ const Login = () => {
           type="text"
           placeholder="Email"
           className="w-full mb-3 p-2 border rounded"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
         <input
           type="password"
           placeholder="Password"
           className="w-full mb-3 p-2 border rounded"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
-        <button className="w-full bg-blue-600 text-white p-2 rounded">
+        <button 
+            className="w-full bg-blue-600 text-white p-2 rounded"
+            onClick={handleLogIn}
+        >
           Login
         </button>
       </div>
---
+++ frontend/src/services/api.js
@@ -0,0 +1,14 @@
+
+const BASE_URL = "http://localhost:5000/api";
+
+export const loginApi = async (data) => {
+  const res = await fetch(`${BASE_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  return res.json();
+};
