f1b262592cacd2ebb10fe5afcc0b95ddb6278dae
shameer <shameer.myd@gmail.com>
2026-04-05 02:10:09 +0530

    Add course details page with dynamic routing

--- frontend/src/pages/CourseDetails.jsx
+++ frontend/src/pages/CourseDetails.jsx
@@ -4,10 +4,34 @@ import { courses } from "../data/courses";
 const CourseDetails = () => {
     const { id } = useParams();
     const course = courses.find((c) => c.id === parseInt(id));
-
     console.log(course);
+
+    if (!course) {
+        return <p className="p-6">Course not found</p>;
+    }
+
     return (
-        <div className="p-6">
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <div className="max-w-xl w-full bg-white border p-6 rounded shadow">
+                <h1 className="text-2xl font-bold">{course.title}</h1>
+
+                <p className="text-blue-600 mt-2">{course.category}</p>
+
+                <p className="mt-4 text-gray-700">{course.description}</p>
+
+                <button className="mt-6 bg-green-600 text-white px-4 py-2 rounded">
+                    Enroll Now
+                </button>
+            </div>
+        </div>
+    )
+
+
+}
+export default CourseDetails;
+
+
+{/* <div className="p-6">
             <h1 className="text-2xl font-bold">{course.title}</h1>
 
             <p className="text-gray-600 mt-2">{course.category}</p>
@@ -17,9 +41,4 @@ const CourseDetails = () => {
             <button className="mt-6 bg-green-600 text-white px-4 py-2 rounded">
                 Enroll Now
             </button>
-        </div>
-    )
-
-
-}
-export default CourseDetails;
+        </div> */}
